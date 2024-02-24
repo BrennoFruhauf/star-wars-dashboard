c2e311f2ea81536372199b14a9b3c49948a8d77e
Brenno <brennofruhauf@outlook.com>
2024-02-24 00:45:29 -0300

    feat: add root layout config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import './globals.css'
 
+import { ReactNode } from 'react'
+
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+
+import MainContent from '@/components/MainContent/MainContent'
+import SideBar from '@/components/SideBar/SideBar'
 
 const font = Poppins({
 	weight: ['100', '300', '500', '700', '900'],
@@ -9,7 +14,7 @@ const font = Poppins({
 })
 
 export const metadata: Metadata = {
-	title: 'Star Wars',
+	title: 'Star Wars | Dashboard',
 	description:
 		'Explore the Star Wars Universe with our interactive dashboard. Dive into detailed profiles of characters, vehicles, species, and planets. Discover the saga like never before!',
 }
@@ -17,11 +22,14 @@ export const metadata: Metadata = {
 export default function RootLayout({
 	children,
 }: Readonly<{
-	children: React.ReactNode
+	children: ReactNode
 }>) {
 	return (
 		<html lang='en'>
-			<body className={font.className}>{children}</body>
+			<body className={font.className}>
+				<SideBar />
+				<MainContent>{children}</MainContent>
+			</body>
 		</html>
 	)
 }
